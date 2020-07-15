a0cd1126c809333ee2a2af696dbb5ecb9b311d7f
David-Evelyn <bonteanevelyndavid@yahoo.ro>
2020-07-15 12:03:00 +0300

    added props on App js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import Login from './Components/Login/Login';
-
+import React, { Component } from 'react';
+import Coaches from './Components/Coaches/CoachesTable/CoachTable';
+import Events from './Components/Events/EventsPage/EventComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-      <Login></Login>
+class App extends Component {
 
-    </div>
-  );
-}
+  state = {
+    header: ['Coaches', 'Events'],
+    coaches: [
+      { name: 'Shane Steward', email: 'samantha.kennedy@example.com', club: 'Biking Club, Running Club,.. +3' },
+      { name: 'Bessie Robertson', email: 'ken.wright@example.com', club: 'Biking Club, Running Club,.. +20' },
+      { name: 'Jorge Edwards', email: 'penny.nichols@example.com', club: 'Biking Club, Running Club,.. +6' },
+      { name: 'Judith Steward', email: 'scott.gilbert@example.com', club: 'Biking Club, Running Club,.. +8' },
+      { name: 'Arlene Murphy', email: 'nina.hernandez@example.com', club: 'Biking Club, Running Club,.. +20' },
+      { name: 'Regina Flores', email: 'keith.richards@example.com', club: 'Biking Club, Running Club,.. +9' },
+      { name: 'Theresa Alexander', email: 'ramona.lowe@example.com', club: 'Biking Club, Running Club,.. +2' }
+    ],
+    events: [
+      { img: '', title: 'Running for Life', body: 'Ad enim sit commodo laborum mollit. Incididunt Lorem exercitation ad occaecat reprehenderit id.', date: '20.06.2020', time: '09:00 AM', location: 'Suceava Fortress, Main Enter', participants: '123' },
+      { img: '', title: 'Running for Life', body: 'Ad enim sit commodo laborum mollit. Incididunt Lorem exercitation ad occaecat reprehenderit id.', date: '20.06.2020', time: '09:00 AM', location: 'Suceava Fortress, Main Enter', participants: '14' },
+      { img: '', title: 'Running for Life', body: 'Ad enim sit commodo laborum mollit. Incididunt Lorem exercitation ad occaecat reprehenderit id.', date: '20.06.2020', time: '09:00 AM', location: 'Suceava Fortress, Main Enter', participants: '35' },
+      { img: '', title: 'Running for Life', body: 'Ad enim sit commodo laborum mollit. Incididunt Lorem exercitation ad occaecat reprehenderit id.', date: '20.06.2020', time: '09:00 AM', location: 'Suceava Fortress, Main Enter', participants: '23523' },
+      { img: '', title: 'Running for Life', body: 'Ad enim sit commodo laborum mollit. Incididunt Lorem exercitation ad occaecat reprehenderit id.', date: '20.06.2020', time: '09:00 AM', location: 'Suceava Fortress, Main Enter', participants: '34' }
+    ]
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        {/* <Login></Login> */}
+        <Coaches
+          coaches={this.state.coaches}
+        />
+        <Events
+          events={this.state.events}
+        />
+
+      </div>
+    );
+  };
+};
 
 export default App;
