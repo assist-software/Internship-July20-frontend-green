931b75055a4bb63452a39363a342bb9c3697dfb7
David-Evelyn <bonteanevelyndavid@yahoo.ro>
2020-07-15 11:16:49 +0300

    Login on App.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Login.css';
-import loginImage from '../../UI/Images/loginImage.jpg';
+// import loginImage from '../../UI/Images/loginImage.jpg';
 
 const login = () => (
     <div className="login-container">
@@ -10,26 +10,26 @@ const login = () => (
             </div>
             <div className="login-data-container" >
                 <div className="login-text">
-                   <p>WELCOME</p>
+                    <p>WELCOME</p>
                     <h2>Login to your account</h2>
                 </div>
-            
+
                 <form className="login-form">
                     <label for="email">
                         Email Address</label>
-                        <input name="email" type="email" placeholder="enter your email" />
-                    
+                    <input name="email" type="email" placeholder="enter your email" />
+
                     <label for="password">
                         Password </label>
-                        <input name="password" type="text" placeholder="enter your password" />
-                    
+                    <input name="password" type="text" placeholder="enter your password" />
+
                     <input type="submit" value="Login" />
                 </form>
             </div>
             <div>
 
             </div>
-           
+
 
         </div>
         <div className="login-image">
