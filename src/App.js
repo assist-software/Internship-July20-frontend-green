b0fda7b1af0003b2c2aa44ea84af7fc963f2cfa5
David-Evelyn <bonteanevelyndavid@yahoo.ro>
2020-07-15 11:36:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Login from './Components/Login/Login'
+// import Login from './Components/Login/Login';
+import Events from './Components/Events/EventsPage/EventComponent'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Events></Events>
 
     </div>
   );
