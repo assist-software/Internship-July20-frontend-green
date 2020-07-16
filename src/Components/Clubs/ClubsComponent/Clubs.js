3708e1fe2e5af12cda3cb1c85eb4e6549e346903
ralucasorina <51170429+ralucasorina@users.noreply.github.com>
2020-07-16 15:27:44 +0300

    coaches, events landing pages, db.json, add and edit modal onclick

---
+++ src/Components/Clubs/ClubsComponent/Clubs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Club from './SingleClub';
+
+import './Clubs.css';
+
+const clubs = props => {
+
+    return props.clubs.map((club, index) => {
+        return (
+        <Club
+            title={club.title}
+            img={club.img}
+            members={club.members}
+            coach={club.coach}
+        />
+        );
+    });
+};
+
+export default clubs;
