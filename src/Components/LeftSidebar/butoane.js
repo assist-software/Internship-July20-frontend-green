3cc7e3a10bbddeef0f77f7c478a466897b55b0bc
Raluca Sorina <ralucasorina.hladiuc@gmail.com>
2020-07-23 14:55:25 +0200

    buttons fixed, :hover and :active added

--- src/Components/LeftSidebar/butoane.js
+++ src/Components/LeftSidebar/butoane.js
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Leftsidebar.css";
 import { NavLink } from "react-router-dom";
 
-const butoane = () => {
-  return (
-    <div>
-      <div className="menu">
-        <NavLink to="/coaches">
-          <button className="btncoaches" id="text">
-            Coaches
-          </button>
-        </NavLink>
-        <NavLink to="/events">
-          <button className="btnevents" id="text">
-            Events
-          </button>
-        </NavLink>
-        <NavLink to="/clubs">
-          <button className="btnclubs" id="text">
-            Clubs
-          </button>
-        </NavLink>
-        <NavLink to="/athletes">
-          <button className="btnath" id="text">
-            Athletes
-          </button>
-        </NavLink>
-      </div>
+class Butoane extends Component {
+  state = { active: false };
+  handleActiveBtn = (e) => {
+    let active = !this.state.active;
+    this.setState({ active: active });
+  };
+  render() {
+    return (
+      <div>
+        <div className="menu">
+          <NavLink to="/coaches" activeClassName="active" className="menuLink">
+            <button
+              id={this.state.active ? "active" : ""}
+              className="btncoaches"
+              id="text"
+              onClick={this.handleActiveBtn}
+            >
+              Coaches
+            </button>
+          </NavLink>
+          <NavLink to="/events" activeClassName="active" className="menuLink">
+            <button className="btnevents btn" id="text">
+              Events
+            </button>
+          </NavLink>
+          <NavLink to="/clubs" activeClassName="active" className="menuLink">
+            <button className="btnclubs btn" id="text">
+              Clubs
+            </button>
+          </NavLink>
+          <NavLink to="/athletes" activeClassName="active" className="menuLink">
+            <button className="btnath btn" id="text">
+              Athletes
+            </button>
+          </NavLink>
+        </div>
 
-      <button className="btnlog" id="textlog">
-        Logout
-      </button>
-    </div>
-  );
-};
-export default butoane;
+        <button className="btnlog" id="textlog">
+          Logout
+        </button>
+      </div>
+    );
+  }
+}
+export default Butoane;
